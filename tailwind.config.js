c415761f4394b7686f175823fab3a3f34ebe125b
Reak-Muny <virakrules015@gmail.com>
2025-03-02 10:35:00 +0700

    form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ["*.html","*.form","html/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        myfont:[
+          "Sigmar","serif"
+        ]
+      },
+      textColor:{
+        primary:[
+          "RGB(138 180 148)"
+        ]
+      },
+      width:{
+        'width25': '25%',
+        'width80': '80%',
+        'width50': '50%',
+      },
+
+    },
   },
   plugins: [],
 }
